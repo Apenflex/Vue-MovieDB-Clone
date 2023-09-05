cdc3b9d07da02a3897abeb485f62a2635749d19a
Svyatoslav <blackdbf@gmail.com>
2023-09-05 16:20:24 +0300

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+	<header>
+		<div>
+			<h3>hi</h3>
+			<nav>
+				<RouterLink to="/">Home</RouterLink>
+			</nav>
+		</div>
+	</header>
+
+	<RouterView />
+</template>
